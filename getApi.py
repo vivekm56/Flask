f04ec010024d25563eb1b16753fb8948be42b4ac
vivek_mudgal <29491716+vivekm56@users.noreply.github.com>
2020-05-09 00:04:33 +0530

    Update getApi.py
    This API is a basic example of the flask API of the GET method.

--- getApi.py
+++ getApi.py
@@ -1,14 +1,12 @@
 from flask import Flask
 
-
-
 app = Flask(__name__)
 
 
 
-@app.route('/<name>', methods=['GET'])
+@app.route('/<name>', methods=['GET']) #put any name at the end of the url ex:- http://127.0.0.1:5000/name
 def hello_world(name):
 	return 'Hello %s!' % name
 
 
-app.run(debug = True)
+app.run(debug = True) #defaults to false. If set to true, provides a debug information. We can also use host, port, options app.run(debug, host, port,options)
